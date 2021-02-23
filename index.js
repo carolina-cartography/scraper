ae404ec472e263951dd49056b6e4eebca5c3309c
Tomas Roy <hello@minicreative.net>
2021-02-22 23:29:23 -0500

    code comments

--- index.js
+++ index.js
@@ -18,43 +18,58 @@ function airbnbClass(name) {
 
 async function scrape() {
 
-    // Initialize an array for our formatted scrape data
+    // Initialize an object to store our formatted scrape data
     let scrapeData = {};
 
-    // Run a while loop to get listings until we decide to break out
+    // Run a while loop to get listings until we decide to break out...
     let offset = 0;
     while (true) {
-
-        // Get page of listings from Airbnb
         console.log(`Getting Airbnb results with offset ${offset}...`)
-        let response = await axios.get(buildURL("https://www.airbnb.com/", {
+
+        // Setup an Airbnb URL using the 'build-url' package
+        let url = buildURL("https://www.airbnb.com/", {
             path: `s/${AIRBNB_QUERY_URL_NAME}/homes`,
             queryParams: {
                 "search_type": "pagination",
                 "items_offset": offset,
             }
-        }))
+        })
+
+        // Get page of listings from Airbnb
+        let response = await axios.get(url)
         let html = response.data
 
         // Make JSDOM instance from HTML, initialize DOM-aware jQuery instance as '$'
         let dom = new JSDOM(html)
         let $ = (jQuery)(dom.window); // From https://medium.com/@asimmittal/using-jquery-nodejs-to-scrape-the-web-9bb5d439413b
 
-        // Get listings, push relevant data into array
+        // Get listing elements
         let listings = $.find(airbnbClass("listing"))
+
+        // For each listing element...
         let newListings = 0;
         for (let listing of listings) {
 
-            // Get data from listing
+            // Get the listing ID, found in the "target" attribute of the listing link
             let idString = $(listing).find(airbnbClass("link")).attr("target")
             let id = idString.replace("listing_", "")
+
+            // Get data
             let title = $(listing).find(airbnbClass("title")).text()
             let price = $(listing).find(airbnbClass("price")).text()
 
-            // Check if listing is already saved
-            if (scrapeData[id] === undefined) {
+            // Instead of using an array to store listing data, we'll use an object
+            // An object in JS is a "key/value pair", like so: {key: "value"}
+            // If we store listings in an object where their "key" is their "id", 
+            // we can avoid adding the same listing twice
+
+            // Check that we don't already have a key for ID in the scrape data...
+            if (scrapeData[id] === undefined) { 
+
+                // Increment the number of new listings found, so we know when to stop looking
                 newListings = newListings + 1;
 
+                // Add an object containing the data to the scrapeData object using the ID
                 scrapeData[id] = {
                     title: title,
                     price: price,
@@ -66,7 +81,7 @@ async function scrape() {
         if (newListings > 0) {
             console.log(`Found ${newListings} new listings!`)
         } else {
-            break;
+            break; // If there are no new listings, stop looking (break out of the while loop)
         }
 
         // Bump the offset to get the next twenty listings
